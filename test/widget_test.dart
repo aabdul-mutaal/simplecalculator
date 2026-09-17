3064529f1db1d94c827fa92cdf1f28a4f754a5ab
Abdul Mutaal <abdulmutaal399@gmail.com>
2026-09-17 16:21:38 -0500

    added 4 more tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -32,7 +32,7 @@ void main() {
     expect(find.text('Error'), findsOneWidget);
   });
 
-  testWidgets('checks for 2+2=4', (tester) async {
+  testWidgets('checks for 2+2=4', (tester) async { //Test 1
     await tester.binding.setSurfaceSize(const Size(390, 844));
     await tester.pumpWidget(const CalculatorApp());
     //Clicks the buttons
@@ -46,5 +46,72 @@ void main() {
     await tester.pump();
     //Check
     expect(find.text('4'), findsNWidgets(2));
+  });
+
+  testWidgets('checks for 99÷3=33', (tester) async { //Test 2
+    await tester.binding.setSurfaceSize(const Size(390, 844));
+    await tester.pumpWidget(const CalculatorApp());
+    //Clicks the buttons
+    await tester.tap(find.text('9').last);
+    await tester.tap(find.text('9').last);
+    await tester.pump();
+    await tester.tap(find.text('÷').last);
+    await tester.pump();
+    await tester.tap(find.text('3').last);
+    await tester.pump();
+    await tester.tap(find.text('=').last);
+    await tester.pump();
+    //Check
+    expect(find.text('33'), findsNWidgets(1));
+  });
+
+  testWidgets('checks for 5+5=10', (tester) async { //Test 3
+    await tester.binding.setSurfaceSize(const Size(390, 844));
+    await tester.pumpWidget(const CalculatorApp());
+    //Clicks the buttons
+    await tester.tap(find.text('5').last);
+    await tester.pump();
+    await tester.tap(find.text('+').last);
+    await tester.pump();
+    await tester.tap(find.text('5').last);
+    await tester.pump();
+    await tester.tap(find.text('=').last);
+    await tester.pump();
+    //Check
+    expect(find.text('10'), findsNWidgets(1));
+  });
+
+  testWidgets('checks for 25+25=50', (tester) async { //Test 4
+    await tester.binding.setSurfaceSize(const Size(390, 844));
+    await tester.pumpWidget(const CalculatorApp());
+    //Clicks the buttons
+    await tester.tap(find.text('2').last);
+    await tester.tap(find.text('5').last);
+    await tester.pump();
+    await tester.tap(find.text('+').last);
+    await tester.pump();
+    await tester.tap(find.text('2').last);
+    await tester.tap(find.text('5').last);
+    await tester.pump();
+    await tester.tap(find.text('=').last);
+    await tester.pump();
+    //Check
+    expect(find.text('50'), findsNWidgets(1));
+  });
+
+  testWidgets('checks for 9÷9=1', (tester) async { //Test 5
+    await tester.binding.setSurfaceSize(const Size(390, 844));
+    await tester.pumpWidget(const CalculatorApp());
+    //Clicks the buttons
+    await tester.tap(find.text('9').last);
+    await tester.pump();
+    await tester.tap(find.text('÷').last);
+    await tester.pump();
+    await tester.tap(find.text('9').last);
+    await tester.pump();
+    await tester.tap(find.text('=').last);
+    await tester.pump();
+    //Check
+    expect(find.text('1'), findsNWidgets(2));
   });
 }
